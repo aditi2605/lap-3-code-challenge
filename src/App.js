cb126258396576235e0e05855c0e664365b42dea
aditi2605 <99731059+aditi2605@users.noreply.github.com>
2022-08-10 10:12:30 +0000

    create routes and route in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from 'react-router-dom';
+import Form from './components/form';
+import userRepo from './components/userRepo';
+import * as Pages from './Pages';
+
 
 function App() {
   return (
     <>
+      <Routes>
+          <Route path="/" element={<Pages.FormPage />} />
+          <Route path="/userRepo" element={<Pages.userRepoPage />} />
+      </Routes>
     </>
   );
 }
