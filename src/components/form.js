30afe91bb6a10b40e4ae628806dd2f1ed0b80caa
aditi2605 <99731059+aditi2605@users.noreply.github.com>
2022-08-10 09:41:25 +0000

    config folder install webpack-cli-dev-server-plugin

---
+++ src/components/form.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+
+function Form() {
+    const [formData, setFormData] = useState({username: '', password: ''})
+
+ 
+function handleFormSubmit(e){
+  console.log("form target is: ", e.target.username)
+    e.preventDefault()
+    setFormData({username: e.target.username.value, password: e.target.password.value})
+    e.target.username.value = '';
+    e.target.password.value = '';
+
+}
+  return (
+<>
+<form onSubmit={handleFormSubmit}>
+    <input type='text' name='username' placeholder='Enter Your Name' />
+    <input type='password' name='password' placeholder='Enter Your Password' />
+    <input type='submit' className='submitBtn' value='Submit' />
+</form>
+<p role="formDisplay">{formData.username}{formData.password}</p>
+</>
+  )
+}
+
+export default Form;
---
+++ src/components/userRepo.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function userRepo() {
+
+    const [repoList, setrepoList] = useState(['']);
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const getData = async() => {
+            try {
+                const { data } = await axios.get(''),
+                setData(data)
+            } catch (e) {
+                console.warn('Opps. ${e.message}')
+            }
+        }
+        getData()
+    }, [])
+  return (
+    <div>
+      <ul>
+        {data}
+      </ul>
+    </div>
+  )
+}
+
+export default userRepo;
